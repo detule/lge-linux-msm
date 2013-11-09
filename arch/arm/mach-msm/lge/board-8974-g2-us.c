6912308cabe08d466a37e1c987d2a9ab1e816110
Laura Abbott <lauraa@codeaurora.org>
2013-11-08 17:06:40 -0800

    LS980: msm: Remove memory reservation code
    The memory reservation API used for allocate_contiguous_ebi is now
    deprecated in favor of using standard kernel APIs such as
    dma_alloc_coherent and CMA for reserving/allocating memory. Remove
    lots of code.
    
    Change-Id: I5a7c22cce6b4b772dc67d18029e40b4509d3ba40
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm/mach-msm/lge/board-8974-g2-us.c
+++ arch/arm/mach-msm/lge/board-8974-g2-us.c
@@ -63,40 +63,18 @@
 extern int update_preset_lcdc_lut(void);
 #endif // CONFIG_LCD_KCAL
 
-static struct memtype_reserve msm8974_reserve_table[] __initdata = {
-	[MEMTYPE_EBI0] = {
-		.flags	=	MEMTYPE_FLAGS_1M_ALIGN,
-	},
-	[MEMTYPE_EBI1] = {
-		.flags	=	MEMTYPE_FLAGS_1M_ALIGN,
-	},
-};
-
-static int msm8974_paddr_to_memtype(phys_addr_t paddr)
-{
-	return MEMTYPE_EBI1;
-}
-
-static struct reserve_info msm8974_reserve_info __initdata = {
-	.memtype_reserve_table = msm8974_reserve_table,
-	.paddr_to_memtype = msm8974_paddr_to_memtype,
-};
-
 void __init msm_8974_reserve(void)
 {
-	reserve_info = &msm8974_reserve_info;
-	of_scan_flat_dt(dt_scan_for_memory_reserve, msm8974_reserve_table);
+	of_scan_flat_dt(dt_scan_for_memory_reserve, NULL);
 #ifdef CONFIG_MACH_LGE
 	of_scan_flat_dt(lge_init_dt_scan_chosen, NULL);
 #endif
-	msm_reserve();
 	lge_reserve();
 }
 
 static void __init msm8974_early_memory(void)
 {
-	reserve_info = &msm8974_reserve_info;
-	of_scan_flat_dt(dt_scan_for_memory_hole, msm8974_reserve_table);
+	of_scan_flat_dt(dt_scan_for_memory_hole, NULL);
 }
 
 #ifdef CONFIG_LGE_LCD_TUNING
