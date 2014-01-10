63948bbe1316af6192cabe2b251ddfd2c4493f48
detule <ogjoneski@gmail.com>
2014-01-09 22:10:42 -0500

    LS980: board: remove deprecated handle_irq and timer fields from machine desc struct

--- arch/arm/mach-msm/lge/board-8974-g2-us.c
+++ arch/arm/mach-msm/lge/board-8974-g2-us.c
@@ -49,6 +49,7 @@
 //#include "../lpm_resources.h"
 #include "../platsmp.h"
 #include <mach/board_lge.h>
+#include "../pm.h"
 
 #ifdef CONFIG_MACH_MSM8974_G2_VZW
 #include <linux/platform_data/lge_android_usb.h>
@@ -306,8 +307,6 @@ DT_MACHINE_START(MSM8974_DT, "Qualcomm MSM 8974 (Flattened Device Tree)")
 	.map_io = msm8974_map_io,
 	.init_irq = msm_dt_init_irq,
 	.init_machine = msm8974_init,
-	.handle_irq = gic_handle_irq,
-	.timer = &msm_dt_timer,
 	.dt_compat = msm8974_dt_match,
 	.reserve = msm_8974_reserve,
 	.init_very_early = msm8974_init_very_early,
